0957264bc2f865feb041632797b026a1316316b4
Mattia <mattia.careddu@conio.com>
2023-08-17 18:32:07 +0200

    remove comments

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,12 @@ const std = @import("std");
 const bip39 = @import("bip39/bip39.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("WALL-E. Bitcoin Wallet written in Zig\n", .{});
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
     const wordlist = try bip39.WordList.init(allocator, "wordlist/english.txt");
-    // defer wordlist.deinit();
 
     var buffer: [24][]u8 = undefined;
     try bip39.getMnemonic(&buffer, wordlist, allocator);
