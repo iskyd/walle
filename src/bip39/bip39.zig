39c3c1eb30dd37976b308ed60e1cd9615a9cfc6b
Mattia <mattia.careddu@conio.com>
2023-08-18 19:54:46 +0200

    mnemonic to seed

--- src/bip39/bip39.zig
+++ src/bip39/bip39.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
-const FILES = [1][]u8{"wordlist/english.txt"};
-pub const MNEMONIC_LENGTH = 24;
-
 pub const WordList = struct {
     allocator: std.mem.Allocator,
     path: []const u8,
@@ -31,7 +28,11 @@ pub const WordList = struct {
     }
 };
 
-pub fn generateEntropy(buffer: []u8) void {
+pub fn generateEntropy(buffer: []u8, ent: u16) void {
+    if (ent % 32 != 0 or ent < 128 or ent > 256) {
+        std.debug.print("Invalid buffer length: {d}\n", .{buffer.len});
+        unreachable;
+    }
     const rand = std.crypto.random;
     rand.bytes(buffer);
 }
@@ -54,12 +55,21 @@ pub fn generateMnemonic(buffer: [][]const u8, entropy: u264, wordlist: WordList,
     }
 }
 
+pub fn mnemonicToSeed(allocator: std.mem.Allocator, buffer: []u8, mnemonic: [24][]const u8, comptime passphrase: []const u8) !void {
+    const salt = "mnemonic" ++ passphrase;
+    const prf = std.crypto.auth.hmac.sha2.HmacSha512;
+
+    const m: []u8 = try std.mem.join(allocator, " ", &mnemonic);
+    defer allocator.free(m);
+    try std.crypto.pwhash.pbkdf2(buffer, m, salt, 2048, prf);
+}
+
 test "wordlist" {
     const allocator = std.testing.allocator;
     const wordlist = try WordList.init(allocator, "wordlist/test.txt");
     defer wordlist.deinit();
     const words: [2048][]const u8 = wordlist.getWords();
-    try std.testing.expectEqualStrings(words[0], "Test");
+    try std.testing.expectEqualStrings("Test", words[0]);
 }
 
 test "generateMnemonic" {
@@ -79,7 +89,7 @@ test "generateMnemonic" {
     var expected1 = std.mem.split(u8, str1, " ");
     var i: u16 = 0;
     while (expected1.next()) |word| {
-        try std.testing.expectEqualStrings(b1[i], word);
+        try std.testing.expectEqualStrings(word, b1[i]);
         i += 1;
     }
 
@@ -95,7 +105,41 @@ test "generateMnemonic" {
     var expected2 = std.mem.split(u8, str2, " ");
     i = 0;
     while (expected2.next()) |word| {
-        try std.testing.expectEqualStrings(b2[i], word);
+        try std.testing.expectEqualStrings(word, b2[i]);
         i += 1;
     }
+}
+
+test "mnemonicToSeed" {
+    const allocator = std.testing.allocator;
+    const wordlist = try WordList.init(allocator, "wordlist/english.txt");
+    defer wordlist.deinit();
+
+    // Test 1
+    var e1 = [32]u8{ 0b11110101, 0b10000101, 0b11000001, 0b00011010, 0b11101100, 0b01010010, 0b00001101, 0b10110101, 0b01111101, 0b11010011, 0b01010011, 0b11000110, 0b10010101, 0b01010100, 0b10110010, 0b00011010, 0b10001001, 0b10110010, 0b00001111, 0b10110000, 0b01100101, 0b00001001, 0b01100110, 0b11111010, 0b00001010, 0b10011101, 0b01101111, 0b01110100, 0b11111101, 0b10011000, 0b10011101, 0b10001111 };
+    const c1 = generateChecksum(&e1);
+    const ue1 = std.mem.readIntBig(u264, &(e1 ++ c1));
+    var b1: [24][]u8 = undefined;
+    try generateMnemonic(&b1, ue1, wordlist, allocator);
+    defer for (b1) |word| allocator.free(word);
+
+    var s1: [64]u8 = undefined;
+    try mnemonicToSeed(allocator, &s1, b1, "TREZOR");
+    const actualSeed1 = std.mem.readIntBig(u512, &s1);
+    const expectedSeed1: u512 = 102649027874290713724689767472589284055554927022246786148344662858622900049166640615657055568848458507251256416806294161430513004959825609395163960773016;
+    try std.testing.expectEqual(expectedSeed1, actualSeed1);
+
+    // Test 2
+    var e2 = [32]u8{ 0b00000110, 0b01101101, 0b11001010, 0b00011010, 0b00101011, 0b10110111, 0b11101000, 0b10100001, 0b11011011, 0b00101000, 0b00110010, 0b00010100, 0b10001100, 0b11101001, 0b10010011, 0b00111110, 0b11101010, 0b00001111, 0b00111010, 0b11001001, 0b01010100, 0b10001101, 0b01111001, 0b00110001, 0b00010010, 0b11011001, 0b10101001, 0b01011100, 0b10010100, 0b00000111, 0b11101111, 0b10101101 };
+    const c2 = generateChecksum(&e2);
+    const ue2 = std.mem.readIntBig(u264, &(e2 ++ c2));
+    var b2: [24][]u8 = undefined;
+    try generateMnemonic(&b2, ue2, wordlist, allocator);
+    defer for (b2) |word| allocator.free(word);
+
+    var s2: [64]u8 = undefined;
+    try mnemonicToSeed(allocator, &s2, b2, "TREZOR");
+    const actualSeed2 = std.mem.readIntBig(u512, &s2);
+    const expectedSeed2: u512 = 2037984480896257598861395356416471090707367901180776855108909406405800700429254515648977950137835572880251515364603734010368696077549762703902611761338653;
+    try std.testing.expectEqual(expectedSeed2, actualSeed2);
 }
--- src/main.zig
+++ src/main.zig
@@ -9,19 +9,28 @@ pub fn main() !void {
 
     const wordlist = try bip39.WordList.init(allocator, "wordlist/english.txt");
 
-    // const ent: u16 = 256; // Entropy length in bits
-    var entropy: [32]u8 = undefined; // 256/8
-    bip39.generateEntropy(&entropy);
+    const ent: u16 = 256;
+    _ = ent; // Entropy length in bits (at the moment only 256 bit is supported)
+    // var entropy: [ent / 8]u8 = undefined; // 256/8
+    // bip39.generateEntropy(&entropy, ent);
+    var entropy = [32]u8{ 0b00000110, 0b01101101, 0b11001010, 0b00011010, 0b00101011, 0b10110111, 0b11101000, 0b10100001, 0b11011011, 0b00101000, 0b00110010, 0b00010100, 0b10001100, 0b11101001, 0b10010011, 0b00111110, 0b11101010, 0b00001111, 0b00111010, 0b11001001, 0b01010100, 0b10001101, 0b01111001, 0b00110001, 0b00010010, 0b11011001, 0b10101001, 0b01011100, 0b10010100, 0b00000111, 0b11101111, 0b10101101 };
     const checksum = bip39.generateChecksum(&entropy);
     const u_entropy = std.mem.readIntBig(u264, &(entropy ++ checksum));
     std.debug.print("Entropy: {d}\n", .{u_entropy});
 
-    var buffer: [24][]u8 = undefined;
-    try bip39.generateMnemonic(&buffer, u_entropy, wordlist, allocator);
+    var mnemonic: [24][]u8 = undefined;
+    try bip39.generateMnemonic(&mnemonic, u_entropy, wordlist, allocator);
     wordlist.deinit();
-    defer for (buffer) |word| allocator.free(word);
+    defer for (mnemonic) |word| allocator.free(word);
 
-    for (buffer) |word| {
+    for (mnemonic) |word| {
         std.debug.print("{s}\n", .{word});
     }
+
+    var seed: [64]u8 = undefined;
+    try bip39.mnemonicToSeed(allocator, &seed, mnemonic, "TREZOR");
+
+    std.debug.print("Seed: {b}\n", .{seed});
+    const x = std.mem.readIntBig(u512, &seed);
+    std.debug.print("{d}\n", .{x});
 }
