939c3f1d99a42230effa7374cb587916f421f4e6
Mattia <mattia.careddu@conio.com>
2023-09-05 10:31:11 +0200

    unittest

--- unit_test.zig
+++ unit_test.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 pub const bip39 = @import("src/bip39/bip39.zig");
 pub const bip32 = @import("src/bip32/bip32.zig");
+pub const secp256k1 = @import("src/secp256k1/secp256k1.zig");
 
 test {
     std.testing.refAllDecls(@This());
