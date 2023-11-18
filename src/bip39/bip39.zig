84395238054e203d2afb226fd8bd6921d6416755
iskyd <iskyd@proton.me>
2023-11-18 11:35:30 +0100

    mnemonic to seed also for different mnemonic length

--- src/bip39/bip39.zig
+++ src/bip39/bip39.zig
@@ -60,7 +60,7 @@ pub fn generateMnemonic(allocator: std.mem.Allocator, entropy: []u8, wordlist: W
     }
 }
 
-pub fn mnemonicToSeed(allocator: std.mem.Allocator, mnemonic: [24][]const u8, passphrase: []const u8, buffer: []u8) !void {
+pub fn mnemonicToSeed(allocator: std.mem.Allocator, mnemonic: [][]u8, passphrase: []const u8, buffer: []u8) !void {
     var salt = try allocator.alloc(u8, "mnemonic".len + passphrase.len);
     defer allocator.free(salt);
     std.mem.copy(u8, salt[0..], "mnemonic");
@@ -68,7 +68,7 @@ pub fn mnemonicToSeed(allocator: std.mem.Allocator, mnemonic: [24][]const u8, pa
 
     const prf = std.crypto.auth.hmac.sha2.HmacSha512;
 
-    const m: []u8 = try std.mem.join(allocator, " ", &mnemonic);
+    const m: []u8 = try std.mem.join(allocator, " ", mnemonic);
     defer allocator.free(m);
     try std.crypto.pwhash.pbkdf2(buffer, m, salt, 2048, prf);
 }
@@ -140,7 +140,7 @@ test "mnemonicToSeed" {
     defer for (b1) |word| allocator.free(word);
 
     var s1: [64]u8 = undefined;
-    try mnemonicToSeed(allocator, b1, "TREZOR", &s1);
+    try mnemonicToSeed(allocator, &b1, "TREZOR", &s1);
     const actualSeed1 = std.mem.readIntBig(u512, &s1);
     const expectedSeed1: u512 = 102649027874290713724689767472589284055554927022246786148344662858622900049166640615657055568848458507251256416806294161430513004959825609395163960773016;
     try std.testing.expectEqual(expectedSeed1, actualSeed1);
@@ -152,8 +152,20 @@ test "mnemonicToSeed" {
     defer for (b2) |word| allocator.free(word);
 
     var s2: [64]u8 = undefined;
-    try mnemonicToSeed(allocator, b2, "TREZOR", &s2);
+    try mnemonicToSeed(allocator, &b2, "TREZOR", &s2);
     const actualSeed2 = std.mem.readIntBig(u512, &s2);
     const expectedSeed2: u512 = 2037984480896257598861395356416471090707367901180776855108909406405800700429254515648977950137835572880251515364603734010368696077549762703902611761338653;
     try std.testing.expectEqual(expectedSeed2, actualSeed2);
+
+    // Test 3, 12 words mnemonic
+    var e3 = [16]u8{ 0b10001011, 0b10010100, 0b11110100, 0b10111001, 0b01110110, 0b11101111, 0b00111101, 0b10000101, 0b10001000, 0b00111101, 0b00001110, 0b10001111, 0b00001000, 0b01110000, 0b11010001, 0b11110011 };
+    var b3: [12][]u8 = undefined;
+    try generateMnemonic(allocator, &e3, wordlist, &b3);
+    defer for (b3) |word| allocator.free(word);
+
+    var s3: [64]u8 = undefined;
+    try mnemonicToSeed(allocator, &b3, "TREZOR", &s3);
+    const actualSeed3 = std.mem.readIntBig(u512, &s3);
+    const expectedSeed3: u512 = 8255326540855321261463521619631741630217762107517752183727119838575482639221952857292840619887401542272957276023929492122223117173190986974270213638571525;
+    try std.testing.expectEqual(expectedSeed3, actualSeed3);
 }
