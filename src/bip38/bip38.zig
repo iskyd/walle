9d7fe333faa6c4019a1609a234e7dc00150ec54c
iskyd <iskyd@proton.me>
2024-01-27 19:52:26 +0100

    Use privatekey instead of wif for bip38 encrypt

--- src/bip38/bip38.zig
+++ src/bip38/bip38.zig
@@ -8,8 +8,8 @@ const Network = @import("../const.zig").Network;
 const EC_MULTIPLY_FLAG_NO_PREFIX = [2]u8{ 0b00000001, 0b01000010 };
 const FLAG_BYTE = [1]u8{0b11000000};
 
-pub fn encrypt(allocator: std.mem.Allocator, wpk: bip32.WifPrivateKey, passphrase: []const u8) ![58]u8 {
-    const public_key = bip32.generatePublicKey(wpk.key);
+pub fn encrypt(allocator: std.mem.Allocator, privatekey: [32]u8, passphrase: []const u8) ![58]u8 {
+    const public_key = bip32.generatePublicKey(privatekey);
     const address = try bip32.deriveAddress(public_key);
     var addresshash: [32]u8 = undefined;
     std.crypto.hash.sha2.Sha256.hash(&address, &addresshash, .{});
@@ -26,8 +26,8 @@ pub fn encrypt(allocator: std.mem.Allocator, wpk: bip32.WifPrivateKey, passphras
     var hexstrkey2: [32]u8 = undefined;
     var hexdh1: [32]u8 = undefined;
     var hexdh2: [32]u8 = undefined;
-    _ = try std.fmt.bufPrint(&hexstrkey1, "{x}", .{std.fmt.fmtSliceHexLower(wpk.key[0..16])});
-    _ = try std.fmt.bufPrint(&hexstrkey2, "{x}", .{std.fmt.fmtSliceHexLower(wpk.key[16..32])});
+    _ = try std.fmt.bufPrint(&hexstrkey1, "{x}", .{std.fmt.fmtSliceHexLower(privatekey[0..16])});
+    _ = try std.fmt.bufPrint(&hexstrkey2, "{x}", .{std.fmt.fmtSliceHexLower(privatekey[16..32])});
     _ = try std.fmt.bufPrint(&hexdh1, "{x}", .{std.fmt.fmtSliceHexLower(derivedhalf1[0..16])});
     _ = try std.fmt.bufPrint(&hexdh2, "{x}", .{std.fmt.fmtSliceHexLower(derivedhalf1[16..32])});
 
@@ -77,9 +77,7 @@ test "base58_encrypt" {
     const seed = [64]u8{ 0b10111000, 0b01110011, 0b00100001, 0b00101111, 0b10001000, 0b01011100, 0b11001111, 0b11111011, 0b11110100, 0b01101001, 0b00101010, 0b11111100, 0b10111000, 0b01001011, 0b11000010, 0b11100101, 0b01011000, 0b10000110, 0b11011110, 0b00101101, 0b11111010, 0b00000111, 0b11011001, 0b00001111, 0b01011100, 0b00111100, 0b00100011, 0b10011010, 0b10111100, 0b00110001, 0b11000000, 0b10100110, 0b11001110, 0b00000100, 0b01111110, 0b00110000, 0b11111101, 0b10001011, 0b11110110, 0b10100010, 0b10000001, 0b11100111, 0b00010011, 0b10001001, 0b10101010, 0b10000010, 0b11010111, 0b00111101, 0b11110111, 0b01001100, 0b01111011, 0b10111111, 0b10110011, 0b10110000, 0b01101011, 0b01000110, 0b00111001, 0b10100101, 0b11001110, 0b11100111, 0b01110101, 0b11001100, 0b11001101, 0b00111100 };
 
     const epk = bip32.generateExtendedMasterPrivateKey(seed);
-    const wpk = bip32.WifPrivateKey.fromPrivateKey(epk.privatekey, Network.MAINNET, true);
-
-    const encrypted = try encrypt(allocator, wpk, "password");
+    const encrypted = try encrypt(allocator, epk.privatekey, "password");
 
     try std.testing.expectEqualStrings(&encrypted, "6PRPV4a5qWAiCxmxFDZQ1bmMwMzUrT7tfkNm9eZsszoj7VWkHu5aQE3jgA");
 }
