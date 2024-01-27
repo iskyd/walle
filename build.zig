69d3c3908372f50d1430a1d992312d3db3190fef
iskyd <iskyd@proton.me>
2024-01-27 15:04:41 +0100

    Rename executable run-p

--- build.zig
+++ build.zig
@@ -63,7 +63,7 @@ pub fn build(b: *std.Build) void {
 
     // Run p.zig
     const exe_p = b.addExecutable(.{
-        .name = "walle",
+        .name = "wallep",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/p.zig" },
