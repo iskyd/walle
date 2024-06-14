b8037af5981a5a73115469b2895d9a9aedda4377
iskyd <iskyd@proton.me>
2024-06-14 18:09:10 +0200

    wtxid

--- src/tx/tx.zig
+++ src/tx/tx.zig
@@ -145,11 +145,22 @@ pub const Transaction = struct {
         return v;
     }
 
-    pub fn getTxID(self: Transaction) ![64]u8 {
+    pub fn getTXID(self: Transaction) ![64]u8 {
         var totalBytes: usize = encodeTxCap(self, true);
         var encoded = try self.allocator.alloc(u8, totalBytes);
         defer self.allocator.free(encoded);
         try encodeTx(self.allocator, encoded, self, true);
+        const txid = utils.doubleSha256(encoded);
+        var txidhex: [64]u8 = undefined;
+        _ = try std.fmt.bufPrint(&txidhex, "{x}", .{std.fmt.fmtSliceHexLower(&txid)});
+        return txidhex;
+    }
+
+    pub fn getWTXID(self: Transaction) ![64]u8 {
+        var totalBytes: usize = encodeTxCap(self, false);
+        var encoded = try self.allocator.alloc(u8, totalBytes);
+        defer self.allocator.free(encoded);
+        try encodeTx(self.allocator, encoded, self, false);
         const txid = utils.doubleSha256(encoded);
         var txidhex: [64]u8 = undefined;
         _ = try std.fmt.bufPrint(&txidhex, "{x}", .{std.fmt.fmtSliceHexLower(&txid)});
@@ -528,8 +539,21 @@ test "txid" {
     var raw: [1036]u8 = "02000000000103c0483c7c93aaefd5ee008cbec6f114d45d7502ffd8c427e9aac13eec327486730000000000fdffffffdaf971319fa0477b53ea4890c647c755c9a0021265f9fc3661ef0c4b7db6ef330000000000fdffffff01bb0ca2b5819c7b6a173cd36b8807d0809cc8bd3f9d5189a354e51b9f9337b00000000000fdffffff0200e40b54020000001600147218978afd7fd9270bae7595399b6bc1986e7a4eecf0052a01000000160014009724e4053330c337bb803eca1007146282124602473044022034141a0bc3da3adfa9162a8ab8f64eed52c94e7cdbc1aa49b0c1bf699c807b2c022015ff3eed0047ab1a202edd89d49cc9a144abeb20a89ff594b7fc50ca723e28870121029e9c928d39269fb6adf718c34e1754983a4d939ea7012f8fbbe51c6711a4a0a4024730440220571285fdbac00b8828883744503ae30bf846fdab3fa197f843f74ec8b6c8627602206a9aa3a646f5a67f62c04f8a181a63f5d4db37ae4e69af5e7f6912b60170bd9b0121029e9c928d39269fb6adf718c34e1754983a4d939ea7012f8fbbe51c6711a4a0a40247304402206d542feca659eed9a470867e1d820b372f434d2a72688143fe68c4c66671a5e50220782b0bd5884d220a537bf86b438cd40eee0a58d08151eb1757e33286658a86110121029e9c928d39269fb6adf718c34e1754983a4d939ea7012f8fbbe51c6711a4a0a4c8000000".*;
     const tx = try decodeRawTx(allocator, &raw);
     defer tx.deinit();
-    const txid = try tx.getTxID();
+    const txid = try tx.getTXID();
     try std.testing.expectEqualStrings(&expectedtxid, &txid);
+}
+
+test "wtxid" {
+    const expectedtxid = "4d95fff811ffdaefcf9add9217d76f123cdc227581fed44de7c43d75a2aa6ac0".*;
+    const expectedwtxid = "ff63ed0cc02c85a7aef6510625b6897abe1b969293f5db2991804e23f2562df1".*;
+    const allocator = std.testing.allocator;
+    var raw: [450]u8 = "01000000000101438afdb24e414d54cc4a17a95f3d40be90d23dfeeb07a48e9e782178efddd8890100000000fdffffff020db9a60000000000160014b549d227c9edd758288112fe3573c1f85240166880a81201000000001976a914ae28f233464e6da03c052155119a413d13f3380188ac024730440220200254b765f25126334b8de16ee4badf57315c047243942340c16cffd9b11196022074a9476633f093f229456ad904a9d97e26c271fc4f01d0501dec008e4aae71c2012102c37a3c5b21a5991d3d7b1e203be195be07104a1a19e5c2ed82329a56b431213000000000".*;
+    const tx = try decodeRawTx(allocator, &raw);
+    defer tx.deinit();
+    const txid = try tx.getTXID();
+    const wtxid = try tx.getWTXID();
+    try std.testing.expectEqualStrings(&expectedtxid, &txid);
+    try std.testing.expectEqualStrings(&expectedwtxid, &wtxid);
 }
 
 test "encodeTxCap" {
