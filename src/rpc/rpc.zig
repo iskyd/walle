641e2f118567cc182c1bf31a4a9cfc9ad3cd09a3
iskyd <iskyd@proton.me>
2024-06-22 16:52:15 +0200

    tmp work on rcp client

--- src/rpc/rpc.zig
+++ src/rpc/rpc.zig
@@ -15,64 +15,19 @@ const BlockHash = struct {
     id: []u8,
 };
 
-const RpcClient = struct {
-    allocator: std.mem.Allocator,
-    httpClient: std.http.Client,
-    uri: std.Uri,
-    headers: std.http.Client.Request.Headers,
-    rpcId: []const u8,
-    authorization_buffer: []const u8,
-
-    method: std.http.Method,
-
-    pub fn init(allocator: std.mem.Allocator, uri: []const u8, userpassbuff: []const u8, rpcId: []const u8) !RpcClient {
-        const uriParsed = std.Uri.parse(uri) catch unreachable;
-        const client = std.http.Client{ .allocator = allocator };
-
-        const encoder = std.base64.Base64Encoder.init(std.base64.standard_alphabet_chars, null);
-        const authorization_buffer: []u8 = try allocator.alloc(u8, encoder.calcSize(userpassbuff.len));
-        defer allocator.free(authorization_buffer);
-
-        const encoded = encoder.encode(authorization_buffer, userpassbuff);
-        // 7 u8 is for "Basic " and "="
-        const buffer = try allocator.alloc(u8, 7 + encoded.len);
-        _ = try std.fmt.bufPrint(buffer, "Basic {s}=", .{encoded});
-
-        const content_type = std.http.Client.Request.Headers.Value{ .override = "text/plain" };
-        const authorization = std.http.Client.Request.Headers.Value{ .override = buffer };
-        const headers = std.http.Client.Request.Headers{ .content_type = content_type, .authorization = authorization };
-
-        return .{
-            .allocator = allocator,
-            .httpClient = client,
-            .uri = uriParsed,
-            .headers = headers,
-            .rpcId = rpcId,
-            .authorization_buffer = buffer,
-            .method = std.http.Method.POST,
-        };
-    }
-
-    pub fn deinit(self: RpcClient) void {
-        var client: *std.http.Client = @constCast(&self.httpClient);
-        client.deinit();
-
-        self.allocator.free(self.authorization_buffer);
-    }
-
-    fn request(self: RpcClient, allocator: std.mem.Allocator, options: std.http.Client.RequestOptions, body: []const u8) ![]u8 {
-        var client: *std.http.Client = @constCast(&self.httpClient);
-        var req = try client.open(self.method, self.uri, options);
-        defer req.deinit();
-        req.transfer_encoding = .chunked;
-        try req.send();
-        try req.writer().writeAll(body);
-        try req.finish();
-        try req.wait();
-
-        return try req.reader().readAllAlloc(allocator, 8192);
-    }
-};
+fn generateAuth(allocator: std.mem.Allocator, user: []const u8, pass: []const u8) ![]u8 {
+    const buffer = try allocator.alloc(u8, user.len + pass.len + 1);
+    defer allocator.free(buffer);
+    @memcpy(buffer[0..user.len], user);
+    buffer[user.len] = ':';
+    @memcpy(buffer[user.len + 1 ..], pass);
+    const encoder = std.base64.Base64Encoder.init(std.base64.standard_alphabet_chars, '=');
+    const authorization_buffer: []u8 = try allocator.alloc(u8, encoder.calcSize(buffer.len) + 6);
+    _ = encoder.encode(authorization_buffer[6..], buffer);
+    const b = "Basic ".*;
+    @memcpy(authorization_buffer[0..6], &b);
+    return authorization_buffer;
+}
 
 fn generateBody(allocator: std.mem.Allocator, rpcId: []const u8, method: []const u8, params: ?std.ArrayList(RpcParams)) ![]const u8 {
     // Number of chars in rpc body (static ones).
@@ -109,48 +64,66 @@ fn generateBody(allocator: std.mem.Allocator, rpcId: []const u8, method: []const
         }
         _ = paramsJSON.pop();
     }
-    std.debug.print("{s}", .{paramsJSON});
+    // std.debug.print("{s}", .{paramsJSON});
     const buffer = try allocator.alloc(u8, cap);
-    _ = try std.fmt.bufPrint(buffer, "{{\"jsonrpc\":\"1.0\",\"id\":\"{s}\",\"method\":\"{s}\",\"params\":[{s}]}}", .{ rpcId, method, paramsJSON });
+    _ = try std.fmt.bufPrint(buffer, "{{\"jsonrpc\":\"1.0\",\"id\":\"{s}\",\"method\":\"{s}\",\"params\":[]}}", .{ rpcId, method });
     return buffer;
 }
 
-pub fn getBlockCount(allocator: std.mem.Allocator, client: RpcClient) !usize {
-    const rpcMethod = "getblockcount".*;
-
-    const body = try generateBody(allocator, client.rpcId, &rpcMethod, null);
-    defer allocator.free(body);
-
+fn req(client: *std.http.Client, uri: std.Uri, auth: []const u8, body: []const u8) !std.http.Client.Request {
     var server_header_buffer: [1024]u8 = undefined;
-    const response = try client.request(allocator, .{ .headers = client.headers, .server_header_buffer = &server_header_buffer }, body);
-    defer allocator.free(response);
+    const content_type = std.http.Client.Request.Headers.Value{ .override = "text/plain" };
+    const authorization = std.http.Client.Request.Headers.Value{ .override = auth };
+    const headers = std.http.Client.Request.Headers{ .content_type = content_type, .authorization = authorization };
+    var request = try client.open(.POST, uri, .{
+        .server_header_buffer = &server_header_buffer,
+        .headers = headers,
+    });
+    request.transfer_encoding = .chunked;
+    try request.send();
+    try request.writeAll(body);
+    try request.finish();
+    try request.wait();
+    return request;
+}
 
+pub fn getBlockCount(allocator: std.mem.Allocator, client: *std.http.Client, location: []const u8, auth: []const u8) !usize {
+    const uri = try std.Uri.parse(location);
+    const rpcId = "walle".*;
+    const rpcMethod = "getblockcount".*;
+    const body = try generateBody(allocator, &rpcId, &rpcMethod, null);
+    defer allocator.free(body);
+    var request = try req(client, uri, auth, body);
+    defer request.deinit();
+    const response = try request.reader().readAllAlloc(allocator, 8192);
+    defer allocator.free(response);
     const parsed = try std.json.parseFromSlice(BlockCount, allocator, response, .{ .allocate = .alloc_always });
     defer parsed.deinit();
     return parsed.value.result;
 }
 
-pub fn getBlockHash(allocator: std.mem.Allocator, client: RpcClient, blockN: usize) ![]u8 {
-    const rpcMethod = "getblockhash".*;
-
-    var params = std.ArrayList(RpcParams).init(allocator);
-    defer params.deinit();
-    try params.append(RpcParams{ .num = blockN });
-
-    const body = try generateBody(allocator, client.rpcId, &rpcMethod, params);
-    defer allocator.free(body);
-    std.debug.print("{s}", .{body});
-    var server_header_buffer: [1024]u8 = undefined;
-    const response = try client.request(allocator, .{ .headers = client.headers, .server_header_buffer = &server_header_buffer }, body);
-    defer allocator.free(response);
-
-    std.debug.print("{s}", .{response});
-
-    const parsed = try std.json.parseFromSlice(BlockHash, allocator, response, .{ .allocate = .alloc_always });
-    defer parsed.deinit();
-    std.debug.print("{s}", .{parsed.value.result});
-    return parsed.value.result;
-}
+//pub fn getBlockHash(allocator: std.mem.Allocator, client: RpcClient, blockN: usize) ![]u8 {
+//    const rpcMethod = "getblockhash".*;
+//
+//    var params = std.ArrayList(RpcParams).init(allocator);
+//    defer params.deinit();
+//    try params.append(RpcParams{ .num = blockN });
+//
+//    const body = try generateBody(allocator, client.rpcId, &rpcMethod, params);
+//    defer allocator.free(body);
+//    std.debug.print("{s}", .{body});
+//    var server_header_buffer = allocator.alloc(u8, 1024);
+//    defer allocator.free(server_header_buffer);
+//    const response = try client.request(allocator, .{ .headers = client.headers, .server_header_buffer = &server_header_buffer }, body);
+//    defer allocator.free(response);
+//
+//    std.debug.print("{s}", .{response});
+//
+//    const parsed = try std.json.parseFromSlice(BlockHash, allocator, response, .{ .allocate = .alloc_always });
+//    defer parsed.deinit();
+//    std.debug.print("{s}", .{parsed.value.result});
+//    return parsed.value.result;
+//}
 
 // pub fn getBlock(client: RpcClient, hash: [64]u8) !Block {
 //     const params = std.ArrayList([64]u8).init(client.allocator);
@@ -161,19 +134,19 @@ pub fn getBlockHash(allocator: std.mem.Allocator, client: RpcClient, blockN: usi
 //     return parsed.value;
 // }
 
-test "generateBodyNoParams" {
-    const allocator = std.testing.allocator;
-    const uri = "http://localhost:18444".*;
-    const userpassbuff = "walle:password".*;
-    const rpcId = "walle".*;
-    const method = "getblockcount".*;
-    const client = try RpcClient.init(allocator, &uri, &userpassbuff, &rpcId);
-    defer client.deinit();
-    const body = try generateBody(allocator, client.rpcId, &method, null);
-    defer allocator.free(body);
-    const expectedString = "{\"jsonrpc\":\"1.0\",\"id\":\"walle\",\"method\":\"getblockcount\",\"params\":[]}".*;
-    try std.testing.expectEqualStrings(&expectedString, body);
-}
+//test "generateBodyNoParams" {
+//    const allocator = std.testing.allocator;
+//    const uri = "http://localhost:18444".*;
+//    const userpassbuff = "walle:password".*;
+//    const rpcId = "walle".*;
+//    const method = "getblockcount".*;
+//    const client = try RpcClient.init(allocator, &uri, &userpassbuff, &rpcId);
+//    defer client.deinit();
+//    const body = try generateBody(allocator, client.rpcId, &method, null);
+//    defer allocator.free(body);
+//    const expectedString = "{\"jsonrpc\":\"1.0\",\"id\":\"walle\",\"method\":\"getblockcount\",\"params\":[]}".*;
+//    try std.testing.expectEqualStrings(&expectedString, body);
+//}
 
 // test "generateBodyNumericParam" {
 //     const allocator = std.testing.allocator;
@@ -193,17 +166,19 @@ test "generateBodyNoParams" {
 //     try std.testing.expectEqualString(&expectedString, body);
 // }
 
-// test "getBlockCount" {
-//     const allocator = std.testing.allocator;
-//     const uri = "http://0.0.0.0:18444".*;
-//     const userpassbuff = "walle:password".*;
-//     const rpcId = "walle".*;
-//     const client = try RpcClient.init(allocator, &uri, &userpassbuff, &rpcId);
-//     defer client.deinit();
-//     const blockCount = try getBlockCount(allocator, client);
-//     std.debug.print("block: {d}\n", .{blockCount});
-//     try std.testing.expect(blockCount >= 0);
-// }
+test "getBlockCount" {
+    const allocator = std.testing.allocator;
+    var client = std.http.Client{ .allocator = allocator };
+    defer client.deinit();
+    const location = "http://0.0.0.0:18444".*;
+    const user = "walle".*;
+    const pass = "password".*;
+    const auth = try generateAuth(allocator, &user, &pass);
+    defer allocator.free(auth);
+    const blockCount = try getBlockCount(allocator, &client, &location, auth);
+    std.debug.print("block: {d}\n", .{blockCount});
+    try std.testing.expect(blockCount >= 0);
+}
 
 // test "getBlockHash" {
 //     const allocator = std.testing.allocator;
@@ -216,3 +191,13 @@ test "generateBodyNoParams" {
 //     std.debug.print("block hash: {s}", .{blockHash});
 //     try std.testing.expect(blockHash.len > 0);
 // }
+
+test "generateAuth" {
+    const user = "walle".*;
+    const pass = "password".*;
+    const expected = "Basic d2FsbGU6cGFzc3dvcmQ=".*;
+    const allocator = std.testing.allocator;
+    const auth = try generateAuth(allocator, &user, &pass);
+    defer allocator.free(auth);
+    try std.testing.expectEqualStrings(&expected, auth);
+}
